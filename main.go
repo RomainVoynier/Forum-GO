f3c43771f53180e78ac271ff207dc309b2d6e67a
RomainVoynier <romain.voynier@ynov.com>
2026-06-08 15:20:05 +0200

    logo

--- main.go
+++ main.go
@@ -6,8 +6,16 @@ import (
 )
 
 func main() {
-	fs := http.FileServer(http.Dir("html"))
-	http.Handle("/", fs)
+	fs := http.FileServer(http.Dir("."))
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path == "/" || r.URL.Path == "/index.html" {
+			http.ServeFile(w, r, "html/index.html")
+			return
+		}
+		fs.ServeHTTP(w, r)
+	})
+
 	addr := ":8080"
 	log.Println("Starting", addr)
 	log.Fatal(http.ListenAndServe(addr, nil))
